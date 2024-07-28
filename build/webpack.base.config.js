b1f5ea22b23bda5d0230f3cc0d6207311aff1da0
superRun <2595039596@qq.com>
2024-07-29 05:09:28 +0800

    添加cross-env到script脚本，配置dev,prod的webpack.config.js，添加压缩抽取等插件

---
+++ build/webpack.base.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ENV = process.env.NODE_ENV; // 获取脚本路径上的参数
+
+module.exports = {
+  entry: "./src/main.ts",
+  output: {
+    filename: "[name].[chunkhash:8].js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.ico$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/",
+            },
+          },
+        ],
+      },
+      {
+        // 用正则去匹配要用该 loader 转换的 CSS 文件
+        test: /\.css$/,
+        use: [
+          ENV === "prod" ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash:8].css",
+    }),
+    new htmlWebpackPlugin({
+      title: "doudouyu",
+      template: "./public/index.html",
+    }),
+  ],
+};
---
+++ build/webpack.dev.config.js
@@ -0,0 +1,18 @@
+const baseWebpackConfig = require("./webpack.base.config");
+const webpackMerge = require("webpack-merge");
+module.exports = webpackMerge.merge(baseWebpackConfig, {
+  mode: "development", // Webpack 会开启一些开发工具和优化，比如开启热更新、注入环境变量等。
+  stats: {
+    // 控制台输出日志控制
+    assets: true,
+    colors: true,
+    errors: true,
+    errorDetails: true,
+    hash: true,
+  },
+  devtool: "cheap-module-source-map",
+  devServer: {
+    hot: true,
+    port: 3000,
+  },
+});
