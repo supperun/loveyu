2d14ed26ecf9c10af8cb710b0cefe278cbff50af
supperun <2595039596@qq.com>
2024-08-15 00:24:25 +0800

    修改配置

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -4,9 +4,6 @@ const TerserPlugin = require('terser-webpack-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 module.exports = webpackMerge.merge(baseWebpackConfig, {
   mode: 'production', // 或 'production' 或 'none' Webpack 会对代码进行压缩、优化，以及移除一些开发环境下的工具和提示。
-  output: {
-    publicPath: '../dist',
-  },
   optimization: {
     minimize: true,
     minimizer: [
