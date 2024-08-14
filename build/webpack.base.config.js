00e1b6d096ad37c6fbbecf743a4aba50bebd1825
supperun <2595039596@qq.com>
2024-08-14 21:55:56 +0800

    添加publicPath

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ENV = process.env.NODE_ENV; // 获取脚本路径上的参数
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ENV = process.env.NODE_ENV // 获取脚本路径上的参数
 
 module.exports = {
-  entry: "./src/main.ts",
+  entry: './src/main.ts',
   output: {
-    filename: "[name].[chunkhash:8].js",
-    path: path.resolve(__dirname, "../dist"),
+    filename: '[name].[chunkhash:8].js',
+    path: path.resolve(__dirname, '../dist'),
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
       },
       {
         test: /\.ico$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
+              name: '[name].[ext]',
             },
           },
         ],
@@ -37,10 +37,10 @@ module.exports = {
         test: /\.(png|jpg)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "assets/",
+              name: '[name].[ext]',
+              outputPath: 'assets/',
             },
           },
         ],
@@ -49,8 +49,8 @@ module.exports = {
         // 用正则去匹配要用该 loader 转换的 CSS 文件
         test: /\.css$/,
         use: [
-          ENV === "prod" ? MiniCssExtractPlugin.loader : "style-loader",
-          "css-loader",
+          ENV === 'prod' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
         ],
       },
     ],
@@ -58,11 +58,11 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "[name].[chunkhash:8].css",
+      filename: '[name].[chunkhash:8].css',
     }),
     new htmlWebpackPlugin({
-      title: "doudouyu",
-      template: "./public/index.html",
+      title: 'doudouyu',
+      template: './public/index.html',
     }),
   ],
-};
+}
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,9 +1,10 @@
-const baseWebpackConfig = require("./webpack.base.config");
-const webpackMerge = require("webpack-merge");
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const baseWebpackConfig = require('./webpack.base.config')
+const webpackMerge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 module.exports = webpackMerge.merge(baseWebpackConfig, {
-  mode: "production", // 或 'production' 或 'none' Webpack 会对代码进行压缩、优化，以及移除一些开发环境下的工具和提示。
+  mode: 'production', // 或 'production' 或 'none' Webpack 会对代码进行压缩、优化，以及移除一些开发环境下的工具和提示。
+  publicPath: '/assets',
   optimization: {
     minimize: true,
     minimizer: [
@@ -11,4 +12,4 @@ module.exports = webpackMerge.merge(baseWebpackConfig, {
       new TerserPlugin({ extractComments: false, test: /\.js(\?.*)?$/i }),
     ],
   },
-});
+})
