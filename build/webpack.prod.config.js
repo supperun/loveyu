90ea12ee309a8a811f627c8266326d8741683106
supperun <2595039596@qq.com>
2024-08-14 22:13:07 +0800

    添加publicPath

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -5,7 +5,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 module.exports = webpackMerge.merge(baseWebpackConfig, {
   mode: 'production', // 或 'production' 或 'none' Webpack 会对代码进行压缩、优化，以及移除一些开发环境下的工具和提示。
   output: {
-    publicPath: '/dist/assets',
+    publicPath: '/dist',
   },
   optimization: {
     minimize: true,
