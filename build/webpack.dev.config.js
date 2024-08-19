8e8ab9e893e16ded8457fdde4dc20cbaa279ec61
supperun <2595039596@qq.com>
2024-08-20 06:58:59 +0800

    爱心鱼小游戏继续维护

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,9 +1,9 @@
-const baseWebpackConfig = require("./webpack.base.config");
-const webpackMerge = require("webpack-merge");
-const path = require("path");
-const DoudouZipPlugin = require("../lib/doudouZipPlugin");
+const baseWebpackConfig = require('./webpack.base.config')
+const webpackMerge = require('webpack-merge')
+const path = require('path')
+const DoudouZipPlugin = require('../lib/doudouZipPlugin')
 module.exports = webpackMerge.merge(baseWebpackConfig, {
-  mode: "development", // Webpack 会开启一些开发工具和优化，比如开启热更新、注入环境变量等。
+  mode: 'development', // Webpack 会开启一些开发工具和优化，比如开启热更新、注入环境变量等。
   stats: {
     // 控制台输出日志控制
     assets: true,
@@ -19,9 +19,9 @@ module.exports = webpackMerge.merge(baseWebpackConfig, {
         test: /\.ts$/,
         use: [
           {
-            loader: path.resolve("lib/doudouyu.js"),
+            loader: path.resolve('lib/doudouyu.js'),
             options: {
-              target: "doudou",
+              target: 'doudou',
             },
           },
         ],
@@ -30,13 +30,13 @@ module.exports = webpackMerge.merge(baseWebpackConfig, {
   },
   plugins: [
     new DoudouZipPlugin({
-      path: "./",
-      fileName: "doudou.zip",
+      path: './',
+      fileName: 'doudou.zip',
     }),
   ],
-  devtool: "cheap-module-source-map",
+  devtool: 'cheap-module-source-map',
   devServer: {
     hot: true,
     port: 3000,
   },
-});
+})
--- src/youxi/gameData.ts
+++ src/youxi/gameData.ts
@@ -1,47 +1,48 @@
-import { draw } from "./common";
+import { draw } from './common'
 
 export class GameData implements draw {
-  fruitNum = 0;
-  double = 1;
-  score = 0;
-  gameOver = false;
-  alpha = 0;
-  canHeight: number;
-  canWidth: number;
-  cxt1: CanvasRenderingContext2D;
+  fruitNum = 0
+  double = 1
+  score = 0
+  gameStart = false
+  gameOver = false
+  alpha = 0
+  canHeight: number
+  canWidth: number
+  cxt1: CanvasRenderingContext2D
   constructor(
     cxt1: CanvasRenderingContext2D,
     canHeight: number,
     canWidth: number
   ) {
-    this.cxt1 = cxt1;
-    this.canHeight = canHeight;
-    this.canWidth = canWidth;
+    this.cxt1 = cxt1
+    this.canHeight = canHeight
+    this.canWidth = canWidth
   }
   draw(deltaTime: number): void {
-    this.cxt1.save();
-    this.cxt1.shadowBlur = 10;
-    this.cxt1.shadowColor = "white";
+    this.cxt1.save()
+    this.cxt1.shadowBlur = 10
+    this.cxt1.shadowColor = 'white'
     this.cxt1.fillText(
-      "Score: " + this.score,
+      'Score: ' + this.score,
       this.canWidth / 2,
       this.canHeight - 50
-    );
+    )
     if (this.gameOver) {
-      this.alpha += deltaTime * 0.0005;
-      if (this.alpha > 1) this.alpha = 1;
-      this.cxt1.fillStyle = "rgba(255,255,255," + this.alpha + ")";
-      this.cxt1.fillText("Game Over!", this.canWidth / 2, this.canHeight / 2);
+      this.alpha += deltaTime * 0.0005
+      if (this.alpha > 1) this.alpha = 1
+      this.cxt1.fillStyle = 'rgba(255,255,255,' + this.alpha + ')'
+      this.cxt1.fillText('Game Over!', this.canWidth / 2, this.canHeight / 2)
     }
-    this.cxt1.restore();
+    this.cxt1.restore()
   }
   addScore() {
-    this.score += this.fruitNum * 100 * this.double;
-    this.fruitNum = 0;
-    this.double = 1;
+    this.score += this.fruitNum * 100 * this.double
+    this.fruitNum = 0
+    this.double = 1
   }
   reset() {
-    this.fruitNum = 0;
-    this.double = 1;
+    this.fruitNum = 0
+    this.double = 1
   }
 }
